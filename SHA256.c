1b77e0470c4f7e30b46d23d9caca2c6e137237bd
badwulf51 <aronomalley@yahoo.com>
2019-06-28 15:37:00 +0100

    added various functions and loops for hashing

--- SHA256.c
+++ SHA256.c
@@ -3,6 +3,12 @@
 
 void sha256();
 
+uint32_t sig0(uint32_t x);
+uint32_t sig1(uint32_t x);
+
+uint32_t rotr(uint32_t n, uint32_t x);
+uint32_t shr(uint32_t n, uint32_t x);
+
 int main (int argc, char *argv[])
 {
 sha256();
@@ -29,4 +35,47 @@ void sha256(){
     };
 
     uint32_t M[16];  
+
+
+    int t;
+
+    for (t =0; t < 16; t++)
+        W[t] = M[t];
+
+    for (t = 16; t < 64; t++)
+        sig1 (W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16]; 
+
+    // intialise a b c d and e 
+    a = H[0]; b = H[1]; c = H[2]; d = H[3]; 
+    e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+    for (t = 0; t < 64; t++){
+        T1 = h + SIG_1(e) + ch(e, f, g) + K[t] + W[t];
+        T2 = sig_0(a) + MAJ(a, b, c);
+        h = g; 
+        g = f; 
+        f = e; 
+        e = d + T1; 
+        d = c;
+        c = b; 
+        b = a; 
+        a = T1 + T2; 
+
+    }
+}
+
+uint32_t rotr(uint32_t n, uint32_t x){
+    return (x >> n) | (x << (32 - n));
+}
+
+uint32_t shr(uint32_t n, uint32_t x){
+    return (x >> n);
+}
+
+
+uint32_t sig0(uint32_t x){
+    return (rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
+}
+uint32_t sig1(uint32_t x){
+    return (rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
 }
