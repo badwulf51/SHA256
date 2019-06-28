d032b07c67613081acd81a49f61e8b34715deb2b
badwulf51 <aronomalley@yahoo.com>
2019-06-28 14:24:33 +0100

    added variables some left unintialsied

--- SHA256.c
+++ SHA256.c
@@ -5,10 +5,28 @@ void sha256();
 
 int main (int argc, char *argv[])
 {
-    
+sha256();
+
 return 0;
 }
 
 void sha256(){
-    
+
+    uint32_t W[64];
+    uint32_t a, b, c, d, e, f, g, h;
+
+    uint32_t T1, T2;
+
+    uint32_t H[8] = {
+        0x6a09e667, 
+        0xbb67ae85, 
+        0x3c6ef372, 
+        0xa54ff53a,
+        0x510e527f, 
+        0x9b05688c, 
+        0x1f83d9ab,
+        0x5be0cd19
+    };
+
+    uint32_t M[16];  
 }
