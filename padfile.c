98ef87bfa835c7f11b6a4b82e1aca516227f643a
badwulf51 <aronomalley@yahoo.com>
2019-07-01 19:54:48 +0100

    added more to padding also fixed a slight mathamatical error

--- padfile.c
+++ padfile.c
@@ -11,7 +11,10 @@ int main(int argc, char *argv[]) {
 
 union msgblock M;
 
+uint64_t nobits = 0;
+
 uint64_t nobytes;
+
 
 FILE* f; 
 
@@ -19,9 +22,27 @@ f = fopen(argv[1], "r");
 
 while (!feof(f)) {
     nobytes = fread(M.e, 1, 64, f);
-    printf("%llu\n", nobytes);
+    printf("Read %2llu\n bytes", nobytes);
+    nobits = nobits + (nobytes * 8);
+    if (nobytes < 56) {
+        printf("Found a block with less than 55 bytes\n");
+        M.e[nobytes] = 0x80;
+        while (nobytes < 56){
+            nobytes = nobytes + 1; 
+            M.e[nobytes] = 0x00;
+        }
+        M.s[7] = nobits;
+
+    }
+ 
+
 }
 fclose(f);
 
- return 0; 
+for (int i = 0; i < 64; i++)
+    printf("%x ", M.e[i]);
+    printf("\n"); 
+
+ return 0;
+  
 }
