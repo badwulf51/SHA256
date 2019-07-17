ad89e85313a9075d7a2ba72552ae83c0f2220029
badwulf51 <aronomalley@yahoo.com>
2019-07-17 17:45:31 +0100

    removed some un needed code and debugging statements, general tidy up

--- SHA256.c
+++ SHA256.c
@@ -177,9 +177,13 @@ if (*S == FINISH)
             M->e[i] = 0x80;
         M->s[7] = *nobits;
         *S = FINISH; 
-    }
-    if (S == PAD1) 
+
+        if (S == PAD1) 
         M->e[0] = 0x80;
+
+        return 1; 
+    }
+    
 
     while (S == READ) {
     nobytes = fread(M->e, 1, 64, f);
@@ -208,21 +212,12 @@ if (*S == FINISH)
 
 }
 
-if (S == PAD0 || S == PAD1) {
-        for (i = 0; i < 56; i++) 
-            M->e[i] = 0x80;
-        M->s[7] = nobits;
-    }
-    if (S == PAD1) 
-        M->e[0] = 0x80;
+
 
 fclose(f);
 
-for (int i = 0; i < 64; i++)
-    printf("%x ", M->e[i]);
-    printf("\n"); 
-
- return 0;
+ 
+ return 1;
   
 }
 
